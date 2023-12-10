a720ad793f91037c6cb570af8e916b2d7ae9d98b
CarlosLaraFP <carloslarafp@gmail.com>
2023-12-10 04:55:31 -0800

    Implemented sockets

---
+++ Assets/SocketController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SocketController : MonoBehaviour
+{
+    public XRGrabInteractable startingInteractable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var socket = this.GetComponent<XRSocketInteractor>();
+        startingInteractable.transform.position = socket.transform.position;
+        startingInteractable.transform.rotation = socket.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
